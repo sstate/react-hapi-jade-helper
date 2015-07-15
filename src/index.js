b73634976a88be1ee53de22fe5379ca8f193a962
Richard Torruellas <rtorruellas@gmail.com>
2015-07-15 11:59:22 -0700

    api changes

--- src/index.js
+++ src/index.js
@@ -6,8 +6,8 @@ var serialize = require('serialize-javascript');
 var ReactHelpers = {
   reactLayoutHandler: function (options) {
     options.reply.view(options.jade_layout || 'index', {
-      state_data: options.state_data,
-      reactComponent: React.renderToString(React.createElement(options.layout, {view: options.component, data: serialize(options.state_data)}))
+      layout_data: options.layout_data,
+      reactComponent: React.renderToString(React.createElement(options.layout, {view: options.component, state_data: serialize(options.layout_data.state_data)}))
     });
   }
 };
