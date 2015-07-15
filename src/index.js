4b6a381a5086c7336de13a798cbe4aae9161589c
Richard Torruellas <rtorruellas@gmail.com>
2015-07-15 11:54:43 -0700

    api changes

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 'use strict';
 
 var React = require('react');
+var serialize = require('serialize-javascript');
 
 var ReactHelpers = {
   reactLayoutHandler: function (options) {
-    options.reply.view(options.jadeLayout || 'index', {
-      layoutData: options.layoutData,
-      reactComponent: React.renderToString(React.createElement(options.Layout, {view: options.component, data: options.layoutData}))
+    options.reply.view(options.jade_layout || 'index', {
+      state_data: options.state_data,
+      reactComponent: React.renderToString(React.createElement(options.layout, {view: options.component, data: serialize(options.state_data)}))
     });
   }
 };
